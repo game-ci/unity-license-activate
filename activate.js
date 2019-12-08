175b81a5d61d9115ac18ee4e68bf6818fe92c0ed
MizoTake <mizotake123@gmail.com>
2019-12-08 19:19:50 +0900

    linux launch

--- activate.js
+++ activate.js
@@ -2,7 +2,7 @@ const puppeteer = require('puppeteer')
 const fs = require('fs')
 
 ;(async () => {
-  const browser = await puppeteer.launch()
+  const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']})
   const page = await browser.newPage()
 
   const downloadPath = process.cwd()
